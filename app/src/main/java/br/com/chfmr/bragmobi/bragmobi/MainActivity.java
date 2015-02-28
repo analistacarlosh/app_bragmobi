ef78b315aacdacb18914ad18d4d6314492c8ccf5
analistacarlosh <analista.carlosh@gmail.com>
2015-02-28 12:56:41 -0300

    First commit

---
+++ app/src/main/java/br/com/chfmr/bragmobi/bragmobi/MainActivity.java
@@ -0,0 +1,76 @@
+package br.com.chfmr.bragmobi.bragmobi;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends ActionBarActivity
+        implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn_horarios_de_onibus = (Button) findViewById(R.id.btn_horarios_de_onibus);
+        btn_horarios_de_onibus.setOnClickListener((View.OnClickListener) this);
+
+        Button btn_denuncia_anonima = (Button) findViewById(R.id.btn_denuncia_anonima);
+        btn_denuncia_anonima.setOnClickListener((View.OnClickListener) this);
+
+        Button btn_disk_entrega = (Button) findViewById(R.id.btn_disk_entrega);
+        btn_disk_entrega.setOnClickListener((View.OnClickListener) this);
+
+        Button btn_contato_app = (Button) findViewById(R.id.btn_contato_app);
+        btn_contato_app.setOnClickListener((View.OnClickListener) this);
+    }
+
+    @Override
+    public void onClick(View view){
+
+        switch (view.getId()){
+            case R.id.btn_horarios_de_onibus:
+                Intent it_btn_horarios_de_onibus = new Intent(this, LineBusActivity.class);
+                startActivity(it_btn_horarios_de_onibus);
+                break;
+            case R.id.btn_denuncia_anonima:
+                Intent it_btn_denuncia_anonima = new Intent(this, LineBusActivity.class);
+                startActivity(it_btn_denuncia_anonima);
+                break;
+            case R.id.btn_disk_entrega:
+                Intent it_btn_disk_entrega = new Intent(this, LineBusActivity.class);
+                startActivity(it_btn_disk_entrega);
+                break;
+            case R.id.btn_contato_app:
+                Intent it_btn_contato_app = new Intent(this, LineBusActivity.class);
+                startActivity(it_btn_contato_app);
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
